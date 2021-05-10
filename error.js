3d769bfb94b4078267c0d872c4685f02293dc0fa
= <=>
2021-05-10 21:22:09 +0530

    added fetch and post api request to the node server

---
+++ error.js
@@ -0,0 +1,11 @@
+const API_ERROR = {
+  fname: "Please Provide First Name",
+  lname: "Please Provide Last Name",
+  uname: "Please Provide UserName",
+  address: "Please Provide Address",
+  email: "Please Provide Email",
+  country: "Please Provide Country",
+  state: "Please Provide State",
+};
+
+module.exports = API_ERROR;
--- public/js/form.js
+++ public/js/form.js
@@ -1,8 +1,10 @@
 const submitFormBtn = document.querySelector(".submit_form");
 const result = document.querySelector(".list-group");
 const registForm = document.querySelector("#reg_form");
+const api_error = document.querySelector(".api_error");
+const invalid_field = document.querySelectorAll(".invalid-feedback");
 
-// Fetch data from the server api
+// console.log(newmap);
 const fetchPeople = (async () => {
   try {
     const { data } = await axios.get("/api/people");
@@ -10,8 +12,8 @@ const fetchPeople = (async () => {
       .map((user) => {
         return `<li class="list-group-item d-flex justify-content-between lh-sm">
           <div>
-            <h6 class="my-0">${user.name}</h6>
-            <small class="text-muted">Brief description</small>
+            <h6 class="my-0">${user.fname} ${user.lname}</h6>
+            <small class="text-muted">${user.address}</small>
           </div>
         </li>`;
       })
@@ -25,11 +27,26 @@ const fetchPeople = (async () => {
 const postFormData = async (ev) => {
   ev.preventDefault();
   const fromdata = new FormData(registForm);
-  console.log(fromdata);
   try {
     const { data } = await axios.post("/api/people", fromdata);
+    const newPerson = `
+    <li class="list-group-item d-flex justify-content-between lh-sm">
+          <div>
+            <h6 class="my-0">${data.data.fname} ${data.data.lname}</h6>
+            <small class="text-muted">${data.data.address}</small>
+          </div>
+        </li>
+    `;
+    result.innerHTML += newPerson;
   } catch (err) {
-    console.log("error");
+    let error_msg = err.response.data.msg;
+    Object.keys(error_msg).forEach((key) => {
+      document
+        .querySelectorAll("[data-error =" + key + "]")[0]
+        .classList.remove("invalid-feedback");
+      document.querySelectorAll("[data-error =" + key + "]")[0].textContent =
+        error_msg[key];
+    });
   }
 };
 
